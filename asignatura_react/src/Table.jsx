8b5d81b8b96d3b1bb6a1083f2f3251a670712954
Ivan Thanon <ivanmoreno199518@gmail.com>
2025-02-23 23:55:21 +0000

    CleanUpCode

--- asignatura_react/src/Table.jsx
+++ asignatura_react/src/Table.jsx
@@ -30,6 +30,8 @@ export const writeRows = async (dispatch) => {
   try {
     const response = await fetch("http://localhost:8080/");
     const data = await response.json();
+
+    
     const propiedades = [
       "id",
       "col2",
